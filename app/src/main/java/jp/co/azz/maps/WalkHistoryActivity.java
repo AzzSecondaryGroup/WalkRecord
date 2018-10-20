6fdf7778efd03a7239f9ed98a61fdf857ef58a38
HoshinoRyota <azz.rhoshino@gmail.com>
2018-10-20 11:28:09 +0900

    履歴一覧作成分を代理でコミット

--- app/src/main/java/jp/co/azz/maps/WalkHistoryActivity.java
+++ app/src/main/java/jp/co/azz/maps/WalkHistoryActivity.java
@@ -1,15 +1,39 @@
 package jp.co.azz.maps;
 
+import android.app.ListActivity;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
-import jp.co.azz.maps.R;
-
-public class WalkHistoryActivity extends AppCompatActivity {
+public class WalkHistoryActivity extends ListActivity  implements LoaderManager.LoaderCallbacks<Cursor> {
+    private WalkRecordAdapter mAdapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_walk_history);
+
+        mAdapter = new WalkRecordAdapter(this, null, 0);
+        setListAdapter(mAdapter);
+
     }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(this,
+                WalkRecordContentProvider.CONTENT_URI, null, null, null, "_id DESC");
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        mAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mAdapter.swapCursor(null);
+    }
+
 }
