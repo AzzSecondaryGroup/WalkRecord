40156eeb0e2517357f795e0b93277d5c73cac4c5
AZZ\m-kubota <azz.mkubota@gmail.com>
2019-02-09 16:53:13 +0900

    開始時間・終了時間フォーマット修正

--- app/src/main/java/jp/co/azz/maps/AppContract.java
+++ app/src/main/java/jp/co/azz/maps/AppContract.java
@@ -4,7 +4,9 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class AppContract {
-    private static final String DATE_FORMAT = "yyyy年MM月dd日 hh時mm分";
+    private static final String DATE_FORMAT = "yyyy-MM-dd HH:mm";
+//    private static final String DATE_FORMAT = "yyyy年MM月dd日 hh時mm分";
+
 
    public static String now() {
        SimpleDateFormat sdf = new SimpleDateFormat(AppContract.DATE_FORMAT);
